5b9403099c34708988051e613a85aa9bf3592ee4
olivier <olivier@oclock.io>
2021-03-01 10:42:38 +0100

    ajout script SQL

---
+++ docs/create_table.sql
@@ -0,0 +1,60 @@
+-- On démarre la transaction, en cas d'erreur durant le script
+-- tout est annulé !
+BEGIN;
+
+-- On fait le ménage
+DROP TABLE IF EXISTS "list",
+"card",
+"label",
+"card_has_label";
+
+
+-- On créé les différentes tables
+CREATE TABLE IF NOT EXISTS "list" (
+    "id" SERIAL PRIMARY KEY,
+    "name" TEXT NOT NULL  
+);
+
+CREATE TABLE IF NOT EXISTS "card" (
+    "id" SERIAL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "position" INTEGER NOT NULL,
+    -- ON delete cascade permet de supprimer automatiquement
+    -- toutes les cartes qui sont associées à une liste lors de la
+    -- suppression de cette dernière.
+    "list_id" INTEGER NOT NULL REFERENCES "list" ("id") ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "label" (
+    "id" SERIAL PRIMARY KEY,
+    "name" TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "card_has_label" (
+  "label_id" INTEGER NOT NULL REFERENCES "label"("id") ON DELETE CASCADE,
+  "card_id" INTEGER NOT NULL REFERENCES "card" ("id") ON DELETE CASCADE,
+  PRIMARY KEY ("label_id", "card_id")
+);
+
+INSERT INTO "list" ("name") VALUES
+('TODO'),
+('ON DOING'),
+('DONE');
+
+INSERT INTO "card" ("title", "position", "color", "list_id") VALUES
+('Faire le MCD',1,'ea52ce', 1),
+('Faire page accueil',2,'18449B', 1),
+('Mettre en place le routeur',1,'52ea61',2);
+
+INSERT INTO "label" ("name") VALUES
+('Back'),
+('Front'),
+('Gestion de projet');
+
+INSERT INTO "card_has_label" ("card_id", "label_id") VALUES
+(1, 3),
+(1, 2),
+(2, 3);
+
+COMMIT;
