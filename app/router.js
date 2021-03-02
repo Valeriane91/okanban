162786c85fcb3a16dd67514ae30ec42ca5ce2340
olivier <olivier@oclock.io>
2021-03-02 12:22:03 +0100

    ajout CRUD

--- app/router.js
+++ app/router.js
@@ -4,5 +4,9 @@ const listController = require('./controllers/listController');
 
 router.get('/lists', listController.getAll);
 router.get('/lists/:id', listController.getOneById);
+router.post('/lists', listController.create);
+router.patch('/lists/:id', listController.update);
+router.put('/lists/:id', listController.updatePut);
+router.delete('/lists/:id', listController.delete);
 
 module.exports = router;
